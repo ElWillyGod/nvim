1ef9e2a2a70ac14558b3dc1c33dcb22b62b1f231
root <manuelantognazza@gmail.com>
2024-03-06 13:31:22 -0300

    modificaiones en el maps

--- lua/willygod/maps.lua
+++ lua/willygod/maps.lua
@@ -28,9 +28,9 @@ map("n", "<leader>q", "<CMD>q<CR>")
 
 -- Exit insert mode
 map("i", "jk", "<ESC>")
-
+ 
 -- Windows
-map("n", "<leader>ñ", "<CMD>vsplit<CR>")
+map("n", "<leader>[", "<CMD>vsplit<CR>")
 map("n", "<leader>p", "<CMD>split<CR>")
 
 -- NeoTree
@@ -43,17 +43,17 @@ map("n", "<S-TAB>", "<CMD>bprevious<CR>")
 
 -- Terminal
 map("n", "<leader>th", "<CMD>ToggleTerm size=10 direction=horizontal<CR>")
-map("n", "<leader>tv", "<CMD>ToggleTerm size=80 direction=vertical<CR>")
+map("n", "<leader>tv", "<CMD>ToggleTer:m size=80 direction=vertical<CR>")
 
 -- Markdown Preview
 map("n", "<leader>m", "<CMD>MarkdownPreview<CR>")
 map("n", "<leader>mn", "<CMD>MarkdownPreviewStop<CR>")
 
 -- Window Navigation
-map("n", "<C-h>", "<C-w>h")
-map("n", "<C-l>", "<C-w>l")
+map("n", "<leader>l", "<C-w>l")
+map("n", "<leader>k", "<C-w>h")
 map("n", "<C-k>", "<C-w>k")
-map("n", "<C-j>", "<C-w>j")
+map("n", "<C-l>", "<C-w>j")
 
 -- Resize Windows
 map("n", "<C-Left>", "<C-w><")
